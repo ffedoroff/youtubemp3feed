f1c92edcb392509f836b0c45667715563a4cf86b
Roman Fedorov <rfedorov@renderedsource.com>
2013-03-09 20:04:15 +0700

    add init version

---
+++ install.sh
@@ -0,0 +1,49 @@
+sudo apt-get install ffmpeg
+sudo apt-get install libavcodec-extra-52 libavdevice-extra-52 libavfilter-extra-0 libavformat-extra-52 libavutil-extra-49 libpostproc-extra-51 libswscale-extra-0
+
+python youtube-dl -x --audio-format mp3 --prefer-free-formats --get-filename -o "%(id)s-%(title)s.%(ext)s" --playlist-start 10 http://www.youtube.com/playlist?list=PL090634BB17919603
+
+python youtube-dl -x --audio-format mp3 --prefer-free-formats -c -t --playlist-start 10 http://www.youtube.com/playlist?list=PL090634BB17919603
+
+
+python youtube-dl -x -c -t -w --playlist-start 22 -k --write-info-json http://www.youtube.com/playlist?list=PL090634BB17919603
+
+
+SeRd38qbZvc-Бизнес-секреты - Ника Белоцерковская.webm
+IdQrGkO6GM8-Бизнес-секреты - Евгений Чичваркин.webm
+l6kj7dqGN0Y-Бизнес-секреты - Сергей Матвиенко.webm
+1JFSWUIHGAs-Бизнес-секреты - Евгений Каценельсон.webm
+A12y4HSQ7DE-Бизнес-секреты - Сергей Петров.webm
+ulzJcKjU6ps-Бизнес-секреты - Арас Агаларов.webm
+xXkQ5G823Ys-Бизнес-секреты - Олег Новиков.webm
+oipxvwjK8mE-Бизнес-секреты - Елена Батурина.webm
+dhdsa0L7gw4-Бизнес-секреты - Дмитрий Маликов.mp4
+504IPjTXd34-Бизнес-секреты - Алексей Панферов.webm
+ZuEQe-pixY0-Бизнес-секреты - Максим Ноготков.webm
+REG3VadrGto-Бизнес-секреты - Майкл Калви.webm
+cLtQHtS2W8k-Бизнес-секреты - Федор Овчинников.webm
+jxYVSBiHPwk-Бизнес-секреты - Игорь Лейтис.webm
+-VP7vghsoEU-Бизнес-секреты - Леонид Шутов.webm
+sP0XmK1dlsE-Бизнес-секреты - Вадим Финкельштейн.webm
+o1pS92AG8xQ-Business Secrets - Filip Engelbert.webm
+mg-I818saDg-Business Secrets - Stephen Jennings.webm
+77KijruPOXA-Бизнес-секреты - Арам Мнацаканов.webm
+5JvQuPx6oDI-Бизнес-секреты - Андреас Рай.webm
+s3BCNDe-2pM-Бизнес-секреты - Артем Аветисян.webm
+M57ZyeDXjo4-Бизнес-секреты - Леонид Огородников.webm
+kj7BIqHYUj8-Бизнес-секреты - Сергей Полонский (1).webm
+KgonsWgYDTA-Business Secrets - Guy Laliberte.webm
+1xTUkxNDYpE-Бизнес-секреты - Сергей Полонский (2).webm
+UNxPixUG5Q4-Бизнес-секреты - Кирилл Андросов.webm
+08ijcWUdeLU-Бизнес-секреты - Максим Иванов.webm
+ig1gb2OvXkA-Бизнес-секреты - Вадим Лапин.webm
+VZLJs5ynLFQ-Бизнес-секреты - Ольга Слуцкер.webm
+n-z9FnRXkXY-Бизнес-секреты - Максим Каширин.webm
+jpdYTSbPU_A-Бизнес-секреты - Алексей Репик.mp4
+Lsvwm-6wNVE-Бизнес-секреты - Алла Вербер.webm
+pBB3VY5c1-k-Бизнес-секреты - Андрей Даниленко.webm
+lKtoMEkzchA-Бизнес-секреты - Всеволод Страх.webm
+k1lpX6Xv8as-Бизнес-секреты - Дмитрий Потапенко.webm
+DlpkVo8L59I-Бизнес-секреты - Александр Акопов.webm
+
+
