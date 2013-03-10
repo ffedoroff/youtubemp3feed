20a05c5649b715d0bb55423b94c0ee028166c638
Roman Fedorov <rfedorov@renderedsource.com>
2013-03-10 22:03:48 +0700

    add first version of feed

--- feed.py
+++ feed.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 
-import json
+import json, glob
 from pprint import pprint
 files = sorted(glob.glob("json/Бизнес-секреты/*.json"))
+
+
+out = """
+<html>
+<head><head>
+<body>
+<ul>
+"""
+
 for file in files:
-	print file
+	#print file
 	json_data=open(file)
 	data = json.load(json_data)
 	json_data.close()
+	out += "<li>"
+	out += '<img src="'+data["thumbnail"]+'" />'
+	out += data["number_in_playlist"].zfill(3)+" "+data["short_title"]
+	out += "</li>"
+
+out += """
+</ul>
+</body>
+</html>
+"""
+
+f = open("res.html", 'w')
+f.write(out.encode("utf-8"))
+f.close()
